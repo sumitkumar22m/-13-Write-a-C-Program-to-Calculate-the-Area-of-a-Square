c4d134a46f7324307bebbc42a1f6b872a682991c
sumitkumar22m <118610704+sumitkumar22m@users.noreply.github.com>
2022-11-25 00:21:57 +0530

    Add files via upload

---
+++ #13 Write a C Program to Calculate the Area of a Square/Write a C Program to Calculate the Area of a Square.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,area;
+    printf("\t\tFind the Area of the Square\n");
+    printf("Enter the value of a ( side of a Square)\n");
+    scanf("%d",&a);
+    area=a*a;
+    printf("Area=%d Unit Square",area);
+    getch();
+}
